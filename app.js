798464dab5c40ea6f1cae817fbac95e35d9b48f0
Diaaana03 <88292313+Diaaana03@users.noreply.github.com>
2024-05-27 02:13:24 +0600

    added current month, best and search movies

--- app.js
+++ app.js
@@ -1,23 +1,82 @@
-const API = "90ed1012-b980-4434-9f47-200245f69049";
-const API_popular =
+const API_key = "90ed1012-b980-4434-9f47-200245f69049";
+const API_best =
   "https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_POPULAR_MOVIES&page=1";
 
 const API_search =
   "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
 
-getPopular(API_popular);
+const API_expected =
+  "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_AWAIT_FILMS";
 
-async function getPopular(url) {
-  const response = await fetch(url, {
-    method: "GET",
-    headers: {
-      "X-API-KEY": API,
-      "Content-Type": "application/json",
-    },
-  });
-  const responseData = await response.json();
-  showPopular(responseData);
+const API_current_month =
+  "https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=2024&month=JULY";
+
+const search = document.querySelector(".header__search");
+
+////// Default page
+
+getMovies(API_best);
+
+//////// Search input
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const APIsearch = `${API_search}${search.value}`;
+  if (search.value) {
+    getMovies(APIsearch);
+    search.value = "";
+  } else {
+    alert("Пожалуйста введите название фильма");
+  }
+});
+
+///// Best movies
+
+const btnBest = document.querySelector(".btn_best");
+
+btnBest.addEventListener("click", () => {
+  getMovies(API_best);
+});
+
+///// Expected movies ?
+
+const btnExpected = document.querySelector(".btn_expected");
+
+btnExpected.addEventListener("click", () => {
+  getMovies(API_expected);
+});
+
+/////Current Movies
+
+const currentBtn = document.querySelector(".btn_current_month");
+currentBtn.addEventListener("click", () => {
+  getMovies(API_current_month);
+});
+
+////// Get movies main function
+
+async function getMovies(url) {
+  try {
+    const response = await fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-KEY": API_key,
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const respData = await response.json();
+    console.log(respData);
+    showMovies(respData);
+  } catch (error) {
+    console.error("Failed to fetch movies:", error);
+  }
 }
+
+///// Movie rating
 
 function getClassByRate(vote) {
   if (vote >= 7) {
@@ -29,8 +88,11 @@ function getClassByRate(vote) {
   }
 }
 
-function showPopular(data) {
+//// Display Movies
+
+function showMovies(data) {
   const moviesEl = document.querySelector(".movies");
+
   document.querySelector(".movies").innerHTML = "";
 
   data.items.forEach((movie) => {
@@ -50,13 +112,12 @@ function showPopular(data) {
         <div class="movie__category">${movie.genres.map(
           (genre) => ` ${genre.genre}`
         )}</div>
-        <div class ="movie__year">${movie.year}<div>
         ${
-          movie.ratingKinopoisk &&
+          movie.ratingImdb &&
           `
         <div class="movie__average movie__average--${getClassByRate(
-          movie.ratingKinopoisk
-        )}">${movie.ratingKinopoisk}</div>
+          movie.ratingImdb
+        )}">${movie.ratingImdb}</div>
         `
         }
       </div>
@@ -64,17 +125,3 @@ function showPopular(data) {
     moviesEl.appendChild(movieEl);
   });
 }
-
-const form = document.querySelector("form");
-const search = document.querySelector(".header__search");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const APIsearch = `${API_search}${search.value}`;
-  if (search.value) {
-    getPopular(APIsearch);
-
-    search.value = "";
-  }
-});
