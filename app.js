fa20d1aaeba9c139abae2428c8cabcbbf3931b6a
Diaaana03 <88292313+Diaaana03@users.noreply.github.com>
2024-05-28 01:01:12 +0600

    added all functions, only favourites section left

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const API_key = "90ed1012-b980-4434-9f47-200245f69049";
+const baseUrl = "https://kinopoiskapiunofficial.tech/api/v2.1/films";
 const API_best =
   "https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_POPULAR_MOVIES&page=1";
 
 const API_search =
   "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
 
-const API_expected =
-  "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_AWAIT_FILMS";
+const API_expected = `${baseUrl}/top?type=TOP_AWAIT_FILMS`;
 
 const API_current_month =
   "https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=2024&month=JULY";
@@ -18,9 +18,10 @@ const API_upcoming =
 
 getMovies(API_best);
 
-//////// Search input ?
+//////// Search input
 
 const form = document.querySelector("form");
+const search = document.querySelector(".header__search");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -41,7 +42,7 @@ btnBest.addEventListener("click", () => {
   getMovies(API_best);
 });
 
-///// Expected movies ?
+///// Expected movies
 
 const btnExpected = document.querySelector(".btn_expected");
 
@@ -56,7 +57,7 @@ currentBtn.addEventListener("click", () => {
   getMovies(API_current_month);
 });
 
-////// Upcoming movies ?
+////// Upcoming movies
 
 const upcomingBtn = document.querySelector(".btn_upcoming_movies");
 upcomingBtn.addEventListener("click", () => {
@@ -91,8 +92,10 @@ function getClassByRate(vote) {
     return "green";
   } else if (vote > 5) {
     return "orange";
-  } else {
+  } else if (vote < 5) {
     return "red";
+  } else if (vote === 3.5) {
+    return "blue";
   }
 }
 
@@ -103,7 +106,9 @@ function showMovies(data) {
 
   document.querySelector(".movies").innerHTML = "";
 
-  data.items.forEach((movie) => {
+  const movies = data.items || data.films || data.releases || "No movies found";
+
+  movies.forEach((movie) => {
     const movieEl = document.createElement("div");
     movieEl.classList.add("movie");
     movieEl.innerHTML = `
@@ -121,12 +126,15 @@ function showMovies(data) {
           (genre) => ` ${genre.genre}`
         )}</div> <div class="heart heart-active" id="heart"></div>
         ${
-          movie.ratingImdb &&
-          `
+          (movie.ratingImdb &&
+            `
         <div class="movie__average movie__average--${getClassByRate(
           movie.ratingImdb
-        )}">${movie.ratingImdb}</div>
-        `
+        )}">${movie.ratingImdb || movie.Kinopoisk}</div>
+        `) ||
+          `<div class="movie__average blue movie__average--${getClassByRate(
+            0
+          )}">3.5</div>`
         }
       </div>
         `;
@@ -135,3 +143,29 @@ function showMovies(data) {
 }
 
 /////Favourites movies
+
+moviesSection.addEventListener("click", (event) => {
+  if (event.target.id === "heart") {
+    event.target.classList.toggle("heart-active");
+    const movieElement = event.target.closest(".movie-container");
+
+    if (event.target.classList.contains("heart-active")) {
+      moviesIdArray.push(movieElement.getAttribute("movieId"));
+      localStorage.setItem("id", JSON.stringify(moviesIdArray));
+    } else {
+      moviesIdArray = moviesIdArray.filter(
+        (item) => item !== movieElement.getAttribute("movieId")
+      );
+      localStorage.setItem("id", JSON.stringify(moviesIdArray));
+    }
+  }
+});
+
+favoritesBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  moviesSection.innerHTML = "";
+  moviesIdArray.forEach((id) => {
+    getFavorites(`https://kinopoiskapiunofficial.tech/api/v2.2/films/${id}`);
+  });
+  console.log("favorites");
+});
